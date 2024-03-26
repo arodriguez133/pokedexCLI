adccde6409f4b3c3bb9ae664a9f6785aad9bd832
arodriguez133 <arodriguez133@icloud.com>
2024-03-25 20:46:49 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"pokedex/internal/pokeapi"
+)
+
+func main() {
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+	//startRepl()
+}
